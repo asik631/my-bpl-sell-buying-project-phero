033ecc4cd83020bb1c9a310d9744f20a04ced7d0
Asik <asikhossain592445@gmail.com>
2026-08-13 23:23:09 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
-
+import headerImg from './assets/logo.png'
+import coinImg from './assets/coin.png'
 
 function App() {
 
   return (
     <>
-      <section>
-        <h1>Hello World</h1>
-       <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex gap-2">
-    <input type="text" placeholder="Search" className="input w-24 md:w-auto" />
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+      <section className="max-w-[1200px] m-auto">
+       <div className="navbar">
+          <div className="flex-1">
+            <a c
+            lassName="text-xl">
+              <img src={headerImg} alt="header image" />
+            </a>
+          </div>
+          <div className="flex items-center gap-7">
+            <div>
+              <ul className='flex items-center gap-15'>
+                <li><a href="">Home</a></li>
+                <li><a href="">Fixure</a></li>
+                <li><a href="">Teams</a></li>
+                <li><a href="">Schedule</a></li>
+              </ul>
+            </div>
+            <div className="flex items-center gap-2 border-2px-solid">
+              <span>60000000</span>
+              <span>coin</span>
+              <img src={coinImg} alt="coin image" />
+            </div>
+          </div>
         </div>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
       </section>
     </>
   )
